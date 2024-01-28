1d6a01b81d4da978aae23edba6162864020a1382
Ravi <ravisingh786420@gmail.com>
2024-01-28 13:52:09 +0530

    first commit

---
+++ src/main/java/com/patientdemo/CachingConfig.java
@@ -0,0 +1,11 @@
+package com.patientdemo;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CachingConfig {
+    // Caching configuration (if needed)
+}
+
---
+++ src/main/java/com/patientdemo/PatientHubServiceSpringbootApplication.java
@@ -0,0 +1,14 @@
+package com.patientdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatientHubServiceSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatientHubServiceSpringbootApplication.class, args);
+		//System.out.print("hello word");
+	}
+
+}
